da6659ac690639ab14aca3a548e737d4d7142b40
Truong Chanh Tran <zheninfy@gmail.com>
2018-07-14 23:22:59 +1000

    disable mn for wallet test

--- guides/smartc_auto.sh
+++ guides/smartc_auto.sh
@@ -36,7 +36,7 @@ echo -e "${GREEN}Preparing config file ${NONE}";
 
 sudo mkdir $HOME/.smrt
 
-printf "addnode=220.233.78.249:9887\n\nrpcuser=smartuser\nrpcpassword=$PASSW\nrpcport=9987\nrpcallowip=127.0.0.1\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=54\nexternalip=$EXTIP:9887\nmasternode=1\nmasternodeprivkey=$MNKEY" >  $HOME/.smrtc/smrtc.conf
+printf "addnode=139.99.197.135:9887\n\nrpcuser=smartcuser\nrpcpassword=$PASSW\nrpcport=9987\nrpcallowip=127.0.0.1\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=54\nexternalip=$EXTIP:9887\n#masternode=1\n#masternodeprivkey=$MNKEY" >  $HOME/.smrtc/smrtc.conf
 
 smrtcd
 watch smrtc-cli getinfo
