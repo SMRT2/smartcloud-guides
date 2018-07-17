3b7f65b7f4152e2820f1473c8b553e9d7f5b0f24
Truong Chanh Tran <zheninfy@gmail.com>
2018-07-18 00:26:00 +1000

    added buildwindows

--- guides/smartc_auto.sh
+++ guides/smartc_auto.sh
@@ -38,7 +38,7 @@ echo -e "${GREEN}Preparing config file ${NONE}";
 
 sudo mkdir $HOME/.smrtc
 
-printf "addnode=139.99.197.135:9887\n\nrpcuser=smartcuser\nrpcpassword=$PASSW\nrpcport=9987\nrpcallowip=127.0.0.1\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=54\nexternalip=$EXTIP:9887" >  $HOME/.smrtc/smrtc.conf
+printf "\n\nrpcuser=smartcuser\nrpcpassword=$PASSW\nrpcport=9987\nrpcallowip=127.0.0.1\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=54\nexternalip=$EXTIP:9887" >  $HOME/.smrtc/smrtc.conf
 
 smrtcd
 watch smrtc-cli getinfo
