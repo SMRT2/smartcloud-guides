d110c8025baf0585aa32476aa137156c266dfd31
telostia <telostia@gmail.com>
2018-07-28 09:13:49 +0200

    fix

--- guides/smartc_auto.sh
+++ guides/smartc_auto.sh
@@ -42,7 +42,7 @@ PASSW=`pwgen -1 20 -n`
 echo -e "${GREEN}Preparing config file ${NONE}";
 sudo mkdir $HOME/.smrtc
 
-printf "addnode=139.99.197.135:9887\naddnode=108.224.49.202:9887\naddnode=220.233.78.249:9887\n\nrpcuser=smartcuser$USER\nrpcpassword=$PASSW\nrpcport=9987\nrpcallowip=127.0.0.1\ndaemon=1\nlisten=1\nserver=1\nmaxconnections=54\nexternalip=$EXTIP\nbind=$EXTIP:9887" >  $HOME/.smrtc/smrtc.conf
+printf "addnode=139.99.197.135:9887\naddnode=108.224.49.202:9887\naddnode=220.233.78.249:9887\n\nrpcuser=smartcuser$USER\nrpcpassword=$PASSW\ndaemon=1\nlisten=1" >  $HOME/.smrtc/smrtc.conf
 
 smrtcd
 watch smrtc-cli getinfo
