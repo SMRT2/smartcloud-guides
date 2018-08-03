6d04b6a553bfab3e5b1cc727cdbc76c38d4519b4
telostia <telostia@gmail.com>
2018-08-03 05:44:33 +0200

    updated hash fix for mn activation

--- guides/smartc_auto.sh
+++ guides/smartc_auto.sh
@@ -15,8 +15,6 @@ sudo apt-get install libminiupnpc-dev -y
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
-#get ip lib
-sudo apt install libwww-perl -y
 
 cd
 #remove old files
